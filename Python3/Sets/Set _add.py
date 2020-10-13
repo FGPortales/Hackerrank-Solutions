5d12f7085eecd455654a5f99368b4d38876e1406
FreddyGiPA <fportalesar@gmail.com>
2020-10-13 16:57:31 -0500

    Incomplete

---
+++ Python3/Sets/Set _add.py
@@ -0,0 +1,15 @@
+import sys
+# example: set.add()
+'''
+s = set('Hackerrank')
+print(s)
+s.add('H')
+print(s.add('a'))
+print(s)
+'''
+
+
+
+# N = int(input())
+# se = set(map(str, input().split()))
+# print(len(se))
