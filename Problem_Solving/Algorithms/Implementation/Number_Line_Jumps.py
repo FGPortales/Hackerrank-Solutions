2a0a4e5524f09515577b065335df03f18bc4abb7
FGPortales <fportalesar@gmail.com>
2020-10-15 23:24:01 -0500

    Problem Solved

--- Problem_Solving/Algorithms/Implementation/Number_Line_Jumps.py
+++ Problem_Solving/Algorithms/Implementation/Number_Line_Jumps.py
@@ -1,12 +1,8 @@
-arr = list(map(int,input().split()))
-band = 0
-while(band == 0):
-    if (arr[0]+arr[1]) == (arr[2] + arr[3]):
-        print("YES")
-        band = 1
-    elif ((arr[0] + arr[1]) > (arr[2] + arr[3])) or arr[3] > arr[1]:
-        print("NO")
-        band = 1
-    arr[0] = arr[0] + arr[1]
-    arr[2] = arr[2] + arr[3]
-
+def kangaroo(x1, v1, x2, v2):
+    if v1 > v2 and not (x2 - x1) % (v1 - v2):
+        res = 'YES'
+    else:
+        res = 'NO'
+    return res
+arr = list(map(int, input().split()))
+print(kangaroo(arr[0],arr[1],arr[2],arr[3]))
