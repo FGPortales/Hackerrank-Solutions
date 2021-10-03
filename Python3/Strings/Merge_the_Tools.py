398249fc6ae22d7bfde3b927808d9005de809921
FGPortales <fportalesar@gmail.com>
2021-10-03 04:04:29 -0500

    [IMP] Change variable

--- Python3/Strings/Merge_the_Tools.py
+++ Python3/Strings/Merge_the_Tools.py
@@ -1,9 +1,9 @@
 
-def merge_the_tools(word, k):
-    for i in range(0, len(word), k):
-        print(''.join(sorted(set(word[i:i+k]), key=word[i:i+k].index)))
+def merge_the_tools(string, k):
+    for i in range(0, len(string), k):
+        print(''.join(sorted(set(string[i:i+k]), key=string[i:i+k].index)))
 
 
 if __name__ == '__main__':
-    word, k = input(), int(input())
-    merge_the_tools(word, k)
+    string, k = input(), int(input())
+    merge_the_tools(string, k)
